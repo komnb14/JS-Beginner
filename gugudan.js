6002069b9be1f05e5b975f753ccb21ebdbd9c4e0
komnb14 <komnb14@gmail.com>
2021-05-15 08:56:19 +0900

    new file:   gugudan.js

---
+++ gugudan.js
@@ -0,0 +1,12 @@
+/*let word='날파리'
+
+while(true)
+{
+    let new_word = prompt(word);
+    if(word[word.length - 1] === new_word[0])
+    {
+        word = new_word;
+        continue;
+    }
+    alert('첫글자가 다릅니다.')
+} */
