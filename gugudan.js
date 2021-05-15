24ad76645563f53fe2691c42d4bc770652e4ad7f
komnb14 <komnb14@gmail.com>
2021-05-15 09:34:11 +0900

    modified:   gugudan.js

--- gugudan.js
+++ gugudan.js
@@ -10,3 +10,23 @@ while(true)
     }
     alert('첫글자가 다릅니다.')
 } */
+        let num1 = Math.floor(Math.random()*9) + 1
+        let num2 = Math.floor(Math.random()*9) + 1
+        let result = num1 * num2
+while(true)
+{
+        
+    let question = prompt(num1 + "곱하기" + num2 + "는?");
+    if(result === question)
+    {
+        num1 = Math.floor(Math.random()*9) + 1
+        num2 = Math.floor(Math.random()*9) + 1
+        result = num1 * num2
+        alert('정답입니다.')
+        continue;
+    }
+
+    if(question === null) break;
+    
+    alert('오답입니다.')
+}
